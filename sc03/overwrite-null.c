fdf370aba9330e67f6ce3b358158a8e2332e9645
chihiro hashimoto <chr@chrhsmt.com>
2015-10-12 19:33:01 +0900

    change code

--- sc03/overwrite-null.c
+++ sc03/overwrite-null.c
@@ -3,9 +3,15 @@
 
 int main(int argc, char **argv) {
   char buf[4][8];
+  int i = 0;
+  printf("%i\n", i++);
   strcpy(buf[0], "********");
+  printf("%i\n", i++);
   strcpy(buf[1], buf[0]);
+  printf("%i\n", i++);
   strcpy(buf[2], buf[0]);
+  printf("%i\n", i++);
   strcpy(buf[3], buf[0]);
+  printf("%i\n", i++);
   printf("%s\n", buf[0]);
 }
